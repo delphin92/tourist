0df344b9bab3c45ea7e0398edaea5654a32a517b
delphin92 <delphin92@list.ru>
2020-01-05 19:28:57 +0300

    New API for checking condition

--- tourist-front/src/model/game/utils.ts
+++ tourist-front/src/model/game/utils.ts
@@ -1,17 +1,24 @@
 import {GameState, GameStateModification} from "model/game/gameState";
+import {ConditionType, startCondition, stopCondition} from "model/game/conditions/conditions";
 
 export type ValueModification = (value: number) => number;
+export type GameStatePredicate = (state: GameState) => boolean;
 
 export const combineModifications: (...modifications: GameStateModification[]) => GameStateModification =
     (...modifications: GameStateModification[]) =>
         gameState => modifications.reduce((nextState, modification) => modification(nextState), gameState);
 
-export const when = (predicate: (state: GameState) => boolean) => (
+export const when = (predicate: GameStatePredicate) => (
     thenModification: GameStateModification,
     elseModification: GameStateModification = NEUTRAL_GAME_STATE_MODIFICATION
 ): GameStateModification =>
     state => predicate(state)
         ? thenModification(state)
         : elseModification(state);
+
+export const checkCondition = (condition: ConditionType, predicate: GameStatePredicate): GameStateModification =>
+    state => predicate(state)
+        ? startCondition(condition)(state)
+        : stopCondition(condition)(state);
 
 export const NEUTRAL_GAME_STATE_MODIFICATION: GameStateModification = state => state;
